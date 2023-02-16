f0b82604609d50469a0cc3e007c809ba92508950
bahalek91@gmail.com <bahalek91@gmail.com>
2023-02-16 10:08:52 +0000

    Updates for Week4 fhv trips data

---
+++ models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,27 @@
+{{ config(materialized="view") }}
+
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source("staging", "fhv_tripdata_partitioned") }}
+  where dispatching_base_num is not null 
+)
+
+select 
+
+    {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    cast(PUlocationID as integer) as  pickup_locationid,
+    cast(DOlocationID as integer) as dropoff_locationid,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    cast(SR_Flag as numeric) as sr_flag,
+    dispatching_base_num as dispatching_basenum,
+    Affiliated_base_number as affiliated_basenum,
+    
+from tripdata
+where rn = 1
